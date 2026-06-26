e96ff5cd3ea2024c3a98e87d482d5dfea153849e
tiffanychristella8-crypto <tiffanychristella8@gmail.com>
2026-06-26 13:11:41 +0700

    26/6

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\KULIAH\PA\PROJECT_NL-1\venv
+set VIRTUAL_ENV=D:\SEM 4\PA\PA kelompok\PROJECT_NL\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
