a647cacc3d57c011052f6d1ba58d63fdfca1f665
cipchizyy <dianfera249@gmail.com>
2026-06-26 12:08:14 +0700

    Delete PROJECT_NL directory

--- PROJECT_NL/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from app.models.user import User
-from app.models.room import Room
-from app.models.reservation import Reservation
-from app.models.payment import Payment
-
-__all__ = ["User", "Room", "Reservation", "Payment"]
