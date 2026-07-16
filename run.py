cd4725ad5c0f2aa8741241ad087fd101f43a38f2
cipchizyy <dianfera249@gmail.com>
2026-07-16 13:37:46 +0700

    server

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from app import create_app
 app = create_app(os.getenv("FLASK_ENV", "production"))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='127.0.0.0', port=5000)
